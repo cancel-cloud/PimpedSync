c22dad56a6528719f4eea7b7547527ca59b3e04d
cancel-cloud <Laggis_131@icloud.com>
2022-04-09 10:34:17 +0200

    Feature: Added Interchange for InvseeCommand

---
+++ src/main/kotlin/de/cancelcloud/commands/InvseeInterChange.kt
@@ -0,0 +1,114 @@
+package de.cancelcloud.commands
+
+import de.cancelcloud.PimpedCache
+import de.cancelcloud.PimpedSync
+import de.cancelcloud.database.InventoryContent
+import de.cancelcloud.utils.Base64
+import de.jet.jvm.extension.tryOrNull
+import de.jet.paper.extension.display.notification
+import de.jet.paper.extension.display.ui.buildPanel
+import de.jet.paper.extension.display.ui.skull
+import de.jet.paper.extension.paper.*
+import de.jet.paper.extension.tasky.async
+import de.jet.paper.extension.tasky.sync
+import de.jet.paper.structure.command.InterchangeUserRestriction
+import de.jet.paper.structure.command.StructuredInterchange
+import de.jet.paper.structure.command.completion.buildInterchangeStructure
+import de.jet.paper.structure.command.completion.component.CompletionAsset
+import de.jet.paper.tool.display.message.Transmission
+import de.jet.paper.tool.timing.tasky.TemporalAdvice
+import net.kyori.adventure.text.Component
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import kotlin.time.Duration.Companion.seconds
+
+/**
+ * PERMISSION = PimpedSync.interchange.invsee
+ */
+class InvseeInterChange : StructuredInterchange("invsee", buildInterchangeStructure {
+
+
+    concludedExecution {
+        "§7This §a§lPimpedSync§7 App from §6Cancelcloud§7 was developed with §eJET Framework§7!"
+            .notification(Transmission.Level.INFO, executor)
+            .display()
+    }
+
+    val completionAsset = CompletionAsset(
+        PimpedSync.instance,
+        "PimpedSync",
+        true,
+        //ist die eingabe richtig? => ob der name in der liste enthalten ist
+        check = {input, ignoreCase -> (server.onlinePlayers.map { it.name } + InventoryContent.getAllPlayerNames()).any { it.equals(input, ignoreCase)}},
+        //transformiert die Eingabe zu einem Objekt (Spielername => Spieler)
+        transformer = { input: String -> getPlayer(input) },
+        //welche Eingabe soll die Liste anzeigen
+        generator = { server.onlinePlayers.map { it.name } + InventoryContent.getAllPlayerNames()}
+    )
+
+    branch {
+        addContent("cache")
+
+        branch {
+            addContent("clear", "update")
+
+            concludedExecution {
+                if (getInput(1) == "clear") {
+                    PimpedCache.dataBasePlayers = emptyList()
+                    "§7Cache cleared.".notification(Transmission.Level.INFO, executor).display()
+                } else {
+                    InventoryContent.getAllPlayerNames()
+                    "§7Cache updated.".notification(Transmission.Level.INFO, executor).display()
+                }
+            }
+        }
+    }
+
+
+    branch {
+        addContent(completionAsset)
+        concludedExecution {
+            val executor = this.executor as Player
+            val player = tryOrNull { InventoryContent.PlayerData(getInput(0, completionAsset).uniqueId, getInput(0, completionAsset).name,
+                Base64.itemStackArrayToBase64(getInput(0, completionAsset).inventory.contents as Array<ItemStack>)!!
+            ) } ?: InventoryContent.getPlayerData(getInput(0))
+            val inventory = buildPanel(6, false) {
+                this.label = Component.text("§a§lInventory of ${player!!.name}")
+                this.identity = PimpedSync.identity
+                this.icon = skull(player.user)
+                //set pannel contents
+                Base64.itemStackArrayFromBase64(player.inventory).forEachIndexed { index, itemStack ->
+                    if (itemStack != null){
+                        //index bezieht sich auf das Pannel
+                        this[index + 9] = itemStack
+                    }
+                }
+
+                //If player is online:
+                onClick {
+                    val player = getPlayer(player.user)
+                    if (player != null) {
+                        async(TemporalAdvice.Companion.delayed(.1.seconds)) {
+                            sync {
+                                player.inventory.contents = it.inventoryView.topInventory.contents!!.drop(9).toTypedArray()
+                            }
+                        }
+                    }
+                }
+
+                //If player is offline
+                onClose {
+                    if (getPlayer(player.user) == null) {
+                        InventoryContent.dbRequestOfflinePlayer(getOfflinePlayer(player.name),
+                            it.inventory.contents?.drop(9)?.toTypedArray() as Array<ItemStack>
+                        )
+                    }
+                }
+
+            }
+            inventory.display(executor)
+
+        }
+    }
+
+}, userRestriction = InterchangeUserRestriction.ONLY_PLAYERS)
